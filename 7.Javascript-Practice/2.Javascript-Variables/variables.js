645c242fd5ea2f6f585a76cf95fd99576e78403a
Sanjib Chakraborty <sanjib.chakraborty@ymedialabs.com>
2023-09-04 19:59:56 +0530

    java script variables

---
+++ 7.Javascript-Practice/2.Javascript-Variables/variables.js
@@ -0,0 +1,50 @@
+console.log("Welcome to JS variables");
+
+// Variable Declaration
+var age = 20;
+let money = 100;
+const city = "Bangalore";
+console.log("Age =", age);
+console.log("Money =", money);
+console.log("City =", city);
+
+//! Primitive Data Types
+
+// string
+let fullName = "John Doe";
+let anotherFullName = "Mike Doe";
+console.log(fullName);
+console.log(anotherFullName);
+
+// Number
+let accountBalance = 200;
+console.log(accountBalance);
+
+// Boolean
+let isMarried = false;
+
+// Null
+let pendingTransaction = null;
+
+// Undefined
+let country = undefined;
+
+// Symbol
+let uniqueID = Symbol("id");
+
+//! Non Primitive Data Types
+
+// Object
+let user = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 30,
+};
+
+// Arrays
+let transactionAmount = [100, 20, 50, 40];
+
+// Function
+let greetUser = () => {
+  console.log("Good Morning");
+};
