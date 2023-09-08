c794ef564de846b5b61f951007600a37ab520f33
Sanjib Chakraborty <sanjib.chakraborty@ymedialabs.com>
2023-09-08 10:10:40 +0530

    Implemented OS module

---
+++ 8.NodeJS-Practice/1.NodeJS-Core-Modules/2.OS-Module/os-module.js
@@ -0,0 +1,23 @@
+const os = require("os");
+
+// Get the type os operating system
+
+console.log(os.type());
+
+// get platform
+console.log(os.platform());
+
+// get architecture
+console.log(os.arch());
+
+// get total memory
+console.log(os.totalmem());
+
+// get free memory
+console.log(os.freemem());
+
+// get uptime
+console.log(os.uptime());
+
+// get the owner info
+console.log(os.userInfo());
