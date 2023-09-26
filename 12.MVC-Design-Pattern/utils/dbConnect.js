4457aec115a5793b7e67c5ad088fd07db28b6a2f
Sanjib Chakraborty <sanjib.chakraborty@ymedialabs.com>
2023-09-26 10:16:18 +0530

    Implemented MVC pattern

---
+++ 12.MVC-Design-Pattern/utils/dbConnect.js
@@ -0,0 +1,7 @@
+const mongoose = require("mongoose");
+module.exports = mongoose
+  .connect(
+    "mongodb+srv://inovotek:Y8MtpWcYhID3JlwS@mongodb-demo.lqjq2rn.mongodb.net/school?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("Db connected"))
+  .catch(err => console.log(err.message));
